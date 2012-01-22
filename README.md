3df1f66fe3678feb897942e40e5cfa5578c802cb
Sheridan Rawlins <scr@chromium.org>
2012-01-21 17:35:41 -0800

    Issue 1 - Added ability to move pinned tabs.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Tabby - the extension to make tabs more sane.
 ## Features
 * New Tabs are inserted to the right of the selected tab.
+* Pinned tabs are transported to the currently focused window.
 
+## To do
 
+* Closing the last unpinned tab should transport to the lower window
+  (may need knowledge of window stack).
+* Newly pinned and unpinned/closed tabs should be remembered too.
--- tabby.js
+++ tabby.js
@@ -1,15 +1,40 @@
 (function() {
     /**
+     * Set this to true when debugging to see alerts at the various
+     * callbacks.
+     * @type {boolean}
+     */
+    var DEBUG = false;
+
+    /**
      * Shorthand for chrome.tabs.
      * @type {Object}
      */
     var tabs = chrome.tabs;
 
     /**
+     * Shorthand for chrome.windows.
+     * @type {Object}
+     */
+    var windows = chrome.windows;
+
+    /**
      * Mapping of windowId -> selectedTabId.
      * @type {Object.<integer, integer>}
      */
     var selectedTabs = {};
+
+    /**
+     * The window id of the focused window.
+     * @type {integer}
+     */
+    var focusedWindow = windows.WINDOW_ID_NONE;
+
+    /**
+     * The currently pinned tabs to move around.
+     * @type {Array.<Tab>}
+     */
+    var pinnedTabs = [];
 
     // populate |selectedTabs| for current state across all windows.
     chrome.windows.getAll(null, function(windows) {
@@ -41,4 +66,50 @@
 		    });
 	    }
 	});
-})()
+
+    // TODO(scr) document
+    windows.onFocusChanged.addListener(function(windowID) {
+					   var moveFromWindowId = focusedWindow;
+					   if (focusedWindow !== windows.WINDOW_ID_NONE) {
+					       tabs.query({
+							      pinned: true,
+							      windowId: moveFromWindowId,
+							  }, function(tabArray) {
+							      pinnedTabs = tabArray;
+							      if (DEBUG) {
+								  alert('# pinned tabs from ' +
+									moveFromWindowId +
+									' = ' + pinnedTabs.length);
+								  console.log(pinnedTabs.map(
+										  function(tab) {
+										      return tab.id;
+										  }));
+							      }
+							      tabs.move(pinnedTabs.map(function(tab) {
+											   return tab.id;
+											   }),
+									{
+									    windowId: windowID,
+									    index: 0,
+									}, function(movedTabs) {
+									    movedTabs.map(function(tab) {
+											      tabs.update(tab.id, {
+													      pinned: true,
+													  });
+											  });
+									});
+							  });
+					   }
+					   focusedWindow = windowID;
+					   if (DEBUG)
+					       alert('focusing ' + windowID);
+				       });
+    // TODO(scr) document
+    windows.onRemoved.addListener(function(windowID) {
+				      if (DEBUG) {
+					  alert('removing ' + windowID +
+						', focused=' + focusedWindow);
+				      }
+				  });
+
+})();
